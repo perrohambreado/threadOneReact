efa51cb31771999409892a45dbf3c87771da9538
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-09-27 00:37:44 -0600

    Unión de 2 componentes

--- reactthreadone/src/App.js
+++ reactthreadone/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import logo from './logo.svg';
 import Header from './components/Header';
-import Home from './components/Home';
-
+import HomePage from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/registro" element={<Header />} />
+        <Route path="/login" element={<Header />} />
+      </Routes>
+    </Router>
   );
 }
 
