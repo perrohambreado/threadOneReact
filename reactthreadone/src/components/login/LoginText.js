e493475770932f334406250bb8b2da4c42b8f77e
rexxrm <reginarmsj@gmail.com>
2024-10-08 19:04:24 -0600

    cambios login

--- reactthreadone/src/components/login/LoginText.js
+++ reactthreadone/src/components/login/LoginText.js
@@ -7,7 +7,7 @@ function LoginText() {
             component="section" 
             sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', margin: '1em' }}
         >
-            <Typography variant="h1" component="h1" sx={{ fontFamily: 'Cormorant, serif', marginTop:"100px", color: "black"}}>
+            <Typography variant="h1" component="h1" sx={{ fontFamily: 'Cormorant, serif', marginRight: '20px', color: "black", position: 'relative', marginBottom: '50px'}}>
                 Inicia sesión
             </Typography>
             <Typography variant="body1" sx={{ fontFamily: 'Cormorant, serif', marginBottom: '20px', fontSize:"21.5px", color:"grey" }}>
